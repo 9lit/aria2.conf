efe8800c4873b15fa22fc9ab3fc8f2f77cd7c13a
LongLit <xiongjum@outlook.com>
2024-06-19 18:10:34 +0800

    初始化程序

--- scrape.sh
+++ scrape.sh
@@ -2,62 +2,203 @@
 
 # 使用 tmdb api 对视频文件进行信息刮削
 #获取目标目录, 设置家目录, 打印日志
-target=$1
+TARGET=$1
 HOME=$(dirname "$(realpath -es "$0")")
-source "${HOME}/Core"
-slog "执行脚本文件scrape.sh, 刮削文件$target" 0
+source "${HOME}/core"
+LOG_INFO "执行脚本文件scrape.sh, 刮削文件$TARGET"
+
+# 对字符串进行 16 进制编码
+function urlencode() {
+  local length="${#1}"
+  for (( i = 0; i < length; i++ )); do
+    local c="${1:i:1}"
+    case $c in
+      [a-zA-Z0-9.~_-]) printf "$c" ;;
+    *) printf "$c" | xxd -p -c1 | while read x;do printf "%%%s" "$x";done
+  esac
+done
+}
+
+function curl_get() {
+
+    local url=$1
+    local headers
+    headers="Authorization: Bearer $(printenv tmdb_auth)"
+
+    curl --request GET --url "$url" --header "$headers"
+
+}
+
+cache=$(get_config_multiple scrape nfo_cache)
+function xml() {
+    echo "$1" >> "$cache"
+}
+
+# ((meta))
+function tag() {
+  local node="$1"
+  echo "$meta" | jq -r ".$node"
+}
+
+function xml_completion() {
+    local element="$1"; local text="$2"; local empty="$3"; local attr="$4"
+    content="$empty<${element}${attr}>$text</$element>"
+    xml "$content"
+    LOG_DEBUG "格式化xml $content"
+}
+
+# 将演员信息添加到 xml 文件中的方法
+# ((index))
+function actor_tag() {
+    # 从 tmdb 等接口中获取演员信息
+    local node="$1"
+    tag "guest_stars[$index].$node"
+}
+
+function actor_xml_completion() {
+# 将获取到的演员信息,格式化为 xml 格式
+  local text prefix
+  local empty="    ";
+  declare -A actor=(["name"]="name" ["role"]="character" ["thumb"]="profile_path" ["order"]="order")
+
+  for element in ${!actor[*]}; do
+    node=${actor[$element]}; prefix=""
+    if [ "$element" = "thumb" ]; then prefix=$(get_config_multiple tmdb profile); fi
+    text="${prefix}$(actor_tag $node)"
+    xml_completion "$element" "$text" "$empty"
+  done
+}
+
+function actor() {
+  LOG_INFO "刮削演员信息"
+  meta="$1"; index=0
+  num=$(echo "$meta" | jq -r ".guest_stars | length")
+  if [ "$num" -eq 0 ]; then return 0; fi
+  while [ $index -lt "$num" ]; do
+    xml "  <actor>"; actor_xml_completion; xml "  </actor>"
+    index=$((index + 1))
+  done
+}
+
+## 刮削工作人员信息
+
+function staff_xml_completion() {
+# 获取演员姓名, 并格式化演员信息
+  local element=""; local empty="  ";
+  if [ "$staff_post" = "Writer" ]; then element="credits"; else element="director"; fi
+  LOG_DEBUG "staff_xml_completion() 工作职位 ${element}, 工作人员名字${staff_name}"
+  xml_completion "$element" "$staff_name" "$empty"
+}
+
+function staff_name() {
+#获取工作人员,导演和脚本家的姓名列表
+  local staff_name
+  # local post="$1"
+  name_string=$(echo "$episode_meta" | jq -r '.crew[] | select( .job == "'$staff_post'" ) | .name')
+  IFS=$'\r\n' read -ra ADDR -d $'\0' <<< "$name_string"
+  for staff_name in "${ADDR[@]}"; do staff_xml_completio; done
+
+}
+
+function staff() {
+#刮削工作人员信息主方法
+  LOG_INFO "刮削工作人员信息"
+  local staff_post
+  posts=("Writer" "Director")
+  for staff_post in "${posts[@]}"; do staff_name; done
+}
+
+function base_is_attr() {
+  local attr=""
+  if [ "$element" = "thumb" ]; then
+    thumb_url="$(get_config_multiple tmdb image)$text"
+    attr=" aspect='thumb' preview='${thumb_url}'"
+  elif [ "$element" = "uniqueid" ]; then
+    attr=" type='imdb' default='true'"
+  fi
+  echo "$attr"
+}
+
+function base_xml_completion() {
+    local element="$1"; node="$2"
+    local empty="  "; local attr=""
+    local text
+    text="$(tag $node)"; attr="$(base_is_attr $element)"
+    LOG_DEBUG "xml 标签内容${text}, 属性值${attr}"
+    xml_completion "$element" "$text" "$empty" "$attr"
+
+}
+
+function base() {
+  LOG_INFO "开始刮削元数据的基本信息"
+  meta="$1"
+  declare -A base=(["title"]="name" ["plot"]="overview" ["runtime"]="runtime" ["thumb"]="still_path")
+  base["uniqueid"]="id"; base["aired"]="air_date"
+
+  for element in ${!base[*]}; do
+    LOG_DEBUG "xml 元素名称 ${element}"
+    node=${base[$element]}
+    LOG_DEBUG "元数据中的键 ${node}"
+    base_xml_completion "$element" "$node"
+  done
+}
 
 # 获取文件路径和文件名称
-filename=$(basename "$target")
-dir_name=$(dirname "$target")
+filename=$(basename "$TARGET")
+dir_name=$(dirname "$TARGET")
 
 # 获取剧集名称以及年份信息
-IFS='/'; read -ra tv_name <<<"$target"
-IFS='.'; read -ra name_and_year <<<"${tv_name[-3]}"
-title=${name_and_year[0]}
-year=${name_and_year[-1]}
-if [[ "$title" && "$year" ]]; then slog "剧集名称${title}和年份${year}获取成功" 0; else slog "剧集名称${title}或年份${year}获取失败" 2; exit 0; fi
+IFS='/'; read -ra episode_path <<<"$TARGET"
+episode_folder="${episode_path}[-3]"
+LOG_DEBUG "剧集文件夹为${episode_folder}"
+IFS='.'; read -ra name_and_year <<<"${episode_folder}"
+title=${name_and_year[0]}; year=${name_and_year[-1]}
+if [[ ! "$title" && "$year" ]]; then LOG_DEBUG "电视剧名称${title}, 电视剧年份${year}"  ; exit 0; fi
 
-# 对视频名称进行编码
 title=$(urlencode "$title")
+LOG_DEBUG "对电视剧名称进行编码处理${title}"
 
 # 获取刮削信息. 语言, 缩略图地址以及演员缩略图地址
 lang=$(get_config_multiple tmdb lang)
-tmdb_image=$(get_config_multiple tmdb image)
+LOG_INFO "刮削语言${lang}"
 # 获取剧集的季节号以及剧集号
 se=$(echo "$filename" | grep -oP "\d{2}")
 season=$(echo "$se" | sed -n 1p)
-episodes=$(echo "$se" | sed -n 1p)
+episode=$(echo "$se" | sed -n 2p)
+LOG_DEBUG "剧集季节号${season}, 剧集集号${episode}"
 
 # 获取剧集的 tmdb id, 以及根据 id 号获取当前剧集元数据
 url="https://api.themoviedb.org/3/search/tv?query=${title}&include_adult=false&language=${lang}&year=${year}"
-id=$(echo "$(curl_get "$url")" | jq ".results[0].id")
-if [ "$id" ]; then slog "tv系列 id 为${id}" 0; else slog "tv系列id获取失败,程序退出"; exit 2; fi
-url="https://api.themoviedb.org/3/tv/${id}/season/${season}/episode/${episodes}?language=${lang}"
+id=$("$(curl_get "$url")" | jq ".results[0].id")
+if [ ! "$id" ]; then LOG_DEBUG "剧集 tmdb id ${id}"; exit 2; fi
+url="https://api.themoviedb.org/3/tv/${id}/season/${season}/episode/${episode}?language=${lang}"
+LOG_DEBUG "请求地址${episode_meta}"
 episode_meta="$(curl_get "$url")"
+LOG_DEBUG "剧集元数据${episode_meta}"
 
-slog "开始刮削视频元数据" 0
+LOG_INFO "开始刮削视频元数据"
 xml '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'
 xml '<episodedetails>'
 base "$episode_meta"; staff "$episode_meta"; actor "$episode_meta"
 xml '</episodedetails>'
-slog "视频元数据刮削成功" 0
+LOG_INFO "视频元数据刮削成功"
 
-slog "开始下载集缩略图" 0
+LOG_INFO "开始下载集缩略图"
+tmdb_image=$(get_config_multiple tmdb image)
+LOG_DEBUG "集缩略图下载网址${tmdb_image}"
 thumb_cache=$(get_config_multiple scrape thumb_cache)
-slog "集缩略图地址获取成功 ${thumb_cache}" 0
+LOG_INFO "集缩略图缓存地址 ${thumb_cache}"
 thumb_url="${tmdb_image}$(tag still_path)"
 curl -o "$thumb_cache" "$thumb_url"
-slog "缩略图成功下载成功" 0
+LOG_INFO "缩略图成功下载成功"
 
-slog "开始将将刮削后的信息上传到远程路径中" 0
+LOG_INFO "开始将将刮削后的信息上传到远程路径中"
 nfo_cache=$(get_config_multiple scrape nfo_cache)
+LOG_DEBUG "集元数据缓存地址${nfo_cache}"
 nfo="${dir_name}/${filename/%.*/.nfo}"
+LOG_DEBUG "集元数据上传地址${nfo_cache}"
 thumb="${dir_name}/${filename/%.*/-thumb.jpg}"
-slog "集元数据缓存获取成功, ${nfo_cache}" 0
-slog "集元数据远程地址获取成功, ${nfo}" 0
-slog "缩略图缓存获取成功, ${thumb_cache}" 0
-slog "缩略图远程地址获取成功, ${thumb}" 0
+LOG_DEBUG "集缩略图远程地址, ${thumb}"
 
-moveto "$nfo_cache" "$nfo"; moveto "$thumb_cache" "$thumb"
-slog "集缩略图和集信息成功上传至远程路径"
+upload moveto "$nfo_cache" "$nfo"; upload moveto "$thumb_cache" "$thumb"
+LOG_INFO "集缩略图和集信息成功上传至远程路径"
--- upload.sh
+++ upload.sh
@@ -2,61 +2,80 @@
 
 # 准备初始变量
 HOME=$(dirname "$(realpath -es "$0")")
-source "${HOME}/Core"
+source "${HOME}/core" "$HOME"
+
+LOG_INFO "执行脚本文件upload.sh, 获取文件:$3"
+
 SOURCE_DIR=$3
 
+function upload() {
 
-ulog "执行脚本文件upload.sh, 获取文件:$3" 0
+    local mode=$1 source=$2
+    local target config 
+    
+    target="$(get_config_multiple rclone name):$1"
+    config="$(get_config_multiple rclone config)"
 
-# 获取输入文件信息, 视频文件名称, 后缀名, 集号
+    rclone -vP $mode "$source" "$target" --log-file="$log_file" --config="$config"
+
+    LOG_DEBUG "upload(), 上传模式$mode"
+    LOG_DEBUG "upload(), 本地文件地址 $source"
+    LOG_DEBUG "upload(), 远程路径$target"
+    LOG_DEBUG "upload(), rclone 配置文件路径 $config"
+    LOG_INFO "upload(), 文件成功上传至网盘"
+}
+
 source_file_name=$(basename "$SOURCE_DIR")
+LOG_INFO "源文件名称$source_file_name"
 ext="${source_file_name##*.}"
-episodes=$(echo "$source_file_name" | grep -oP '\[\d{2}\]|-\s\d+|E\d+|\[\d{2}v2\]' | grep -oP '\d+')
+episode=$(echo "$source_file_name" | grep -oP '\[\d{2}\]|-\s\d+|E\d+|\[\d{2}v2\]' | grep -oP '\d+')
 
-if [ "$source_file_name" ]; then ulog "视频文件名称${source_file_name}和后缀名${ext}获取成功" 0; fi
-if [ "$episodes" ]; then ulog "集号:${episodes}获取" 0; else ulog "集号${episodes}获取失败,脚本退出" 2; exit 0;fi
-
-
-IFS=$'\r\n' read -ra tasks -d $"\0" <<< "$(get_task_name)"
+content="视频集号${episode}或者后缀名称${ext}获取失败, 脚本退出"
+if [[ ! "$episode" && "$ext" ]]; then LOG_INFO "$content"; exit 0; fi
 
 # 获取远程路径
+IFS=$'\r\n' read -ra tasks -d $"\0" <<< "$(get_task_name)"
 for task in "${tasks[@]}"; do
+LOG_DEBUG "任务编号${animation}"
 animation=$(get_config_multiple  "$task" video)
+LOG_DEBUG "视频关键词${animation}"
 
 if [[ "$source_file_name"^^ =~ $"$animation" ]]; then
     target_dir=$(get_config_multiple "$task" path)
-    ulog 远程路径["${target_dir}"]获取成功,准备上传文件 0
+    LOG_DEBUG "视频文件的远程上传路径文件夹${target_dir}"
     break
 else
-    target_dir=0
+target_dir=0
 fi
 
 done
 
+LOG_DEBUG "视频文件的远程上传路径文件夹${target_dir}"
 #如果没有获取到目标目录, 则上传至网盘的临时文件夹,并退出脚本
-if [ "$target_dir" == 0 ]; then
-    spare=$(get_config_multiple rclone spare)
-    ulog "文件${source_file_name}不在追番列表中, 上传到远程目录${spare}, 并退出脚本" 1
-    copyto "$SOURCE_DIR" "$spare" 
-    exit 0 
+if [ "$target_dir" -eq 0 ]; then
+  spare=$(get_config_multiple rclone spare)
+  LOG_INFO "文件不在追番列表中,上传到远程临时目录${spare}"
+  upload copyto "$SOURCE_DIR" "$spare"
+  LOG_INFO "文件上传成功"
+  exit 0
 fi
 
-# 向目标目录中获取季节号,并格式化为 "00"
 season=$(basename "$target_dir" | grep -oP '\d+')
+LOG_DEBUG "视频文件的季节号为${season}"
 if [ ${#season} -eq 1 ]; then season="0$season";fi
+LOG_DEBUG "视频文件的季节号格式化为${season}"
 
-# 获取远程路径, 格式化目标文件名称 "S00E00.ext"
-target="$(get_config_multiple rclone target)${target_dir}/S${season}E${episodes}.${ext}"
-ulog "目标地址获取成功:${target}, 准备移动文件" 0
+target="$(get_config_multiple rclone target)${target_dir}/S${season}E${episode}.${ext}"
+LOG_INFO "文件的远程上传路径(最终地址)${target}"
 
-#移动并重命名
-copyto "$SOURCE_DIR" "$target" 
-ulog "文件上传至网盘${target}成功,准备刮削数据" 0
+# 上传文件
+upload copyto "$SOURCE_DIR" "$target" 
 
 # 是否调用 scrape.sh 脚本文件
 if [ "$(get_config_multiple scrape flag)" -eq 0 ]; then 
-    ulog "scrape 的 flage 等于 0, 调用脚本文件 scrape.sh 准备刮削远程视频文件" 0
+    LOG_INFO "scrape 的 flag 等于 0, 调用脚本文件 scrape.sh 准备刮削远程视频文件" 0
     "${HOME}"/scrape.sh "$target"
+    LOG_DEBUG "执行文件地址${HOME}/scrape.sh $target"
 else
-    ulog "scrape 的 flage 不为 0, 退出脚本"
+    LOG_INFO "scrape 的 flae 不为 0, 退出脚本"
 fi
