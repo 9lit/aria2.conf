aabeac6f5c4f50b2d1c0f73565722dabd14ac41a
LongLit <xiongjum@outlook.com>
2024-07-13 16:09:27 +0800

    优化 上传视频脚本, 以及 core 文件,删除不必要的配置, 优化发送邮件功能

--- upload.sh
+++ upload.sh
@@ -8,6 +8,10 @@ ALIST_URL=$(get_config_multiple alist url)
 RCLONE_SPARE=$(get_config_multiple rclone spare)
 RCLONE_TRAGE=$(get_config_multiple rclone target)
 RCLONE_NAME=$(get_config_multiple rclone name)
+EMAIL_SENDER=$(get_config_multiple sendmail sender)
+EMAIL_PASSWD=$(get_config_multiple sendmail passwd)
+EMAIL_RECIPIENT=$(get_config_multiple sendmail recipient)
+EMAIL_FLAG=$(get_config_multiple sendmail flag)
 SOURCE_DIR=$3
 
 function get_file_info() {
@@ -35,10 +39,19 @@ function animation_list() {
 
 function sendmail() {
     # 发送推送成功的电子邮件
-    local flag && flag=$(get_config_multiple sendmail flag)
-    local context && context="<p>[追番信息]$title</p><p>$string</p>"
-    if [ $flag -ne 0 ]; then exit 0; fi
-    /home/kuma/venvs/bin/python3 sendmail.py "$1" "$content"
+    local status
+    if [ $EMAIL_FLAG -ne 0 ]; then exit 0; fi
+    # /home/kuma/venvs/bin/python3 sendmail.py "$title" "$"
+    message=$(python /c/Users/lain/test.py)
+
+    if [ $message == "邮件发送成功！" ]; then status="成功"; else status="失败";fi
+    mailmessage="
+===邮件发送信息
+发件人: $EMAIL_SENDER
+收件人: $EMAIL_RECIPIENT
+是否发送成功: $status
+"
+    LOG "$mailmessage" "$source_file_name"
 }
 
 get_file_info && get_remote_path
@@ -46,20 +59,22 @@ get_file_info && get_remote_path
 #如果没有获取到目标目录, 则上传至网盘的临时文件夹,并退出脚本
 if [ "$target_dir" -eq 0 ]; then 
   rclone_path="${RCLONE_SPARE}/${source_file_name}"
-  url=${ALIST_URL}{$rclone_path} && title="$source_file_name"
+  url="${ALIST_URL}${rclone_path}" && title="$source_file_name"
+  animation_name=""
 else 
   animation_list 
-  url=${ALIST_URL}{$target} && title="$animation_name"
+  url="${ALIST_URL}${target}" && title="$animation_name"
 fi
 
 UPLOAD copyto "$SOURCE_DIR" "${RCLONE_NAME}:${rclone_path}"
 
-string=$(echo << EOF
-===处理视频文件信息
+string="
+===输入视频文件信息
 输入文件路径: $SOURCE_DIR
 文件名称: $source_file_name
 文件后缀: $ext
 视频集号: $episode
+文件大小: $(du -h "$SOURCE_DIR" | cut -d '/' -f1)
 
 ===rclone 远程路径信息
 视频系列名称: $animation_name
@@ -67,13 +82,12 @@ rclone 名称: $RCLONE_NAME
 上传路径: $rclone_path
 
 ===alist 在线观看地址
-$url
-EOF
-)
-
-LOG_INFO "$string"
+$(URLENCODE "$url")
+"
 
 sendmail
+
+LOG "$string" "$source_file_name"
 
 # # 是否调用 scrape.sh 脚本文件
 # if [ "$(get_config_multiple main scrape_flag)" -eq 0 ]; then 
